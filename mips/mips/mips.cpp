1363531a88e9c91d60e18620a82f628097c64af4
miaochenlu <32099670+miaochenlu@users.noreply.github.com>
2019-04-10 14:59:55 +0800

    Add files via upload

---
+++ mips/mips/mips.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int Op2Binary(string op) {
+	int result = 0;
+	transform(op.begin(), op.end(), op.begin(), ::tolower);
+	if (op == "j" || op == "jal") {
+
+	}
+	else if (op == "jr") {
+
+	}
+	else if (op == "addi" || op == "ori" || op == "beq") {
+
+	}
+	else if (op == "add" || op == "sub" || op == "and" || op == "or" || op == "slt" || op == "sll") {
+
+	}
+	else if (op == "sw" || op == "lw") {
+
+	}
+	return result;
+}
+int main() {
+	ifstream fin("mips.txt");
+	if (!fin) {
+		cout << "Can't open the file" << endl;
+		exit(0);
+	}
+	string op;
+	char buffer[256];
+	char **tmp = 0;
+	char *oper;
+	while (!fin.eof()) {
+		fin.getline(buffer, 256);
+		if (buffer[0]) {
+			oper = strtok_s(buffer, " ", tmp);
+			op = oper;
+			cout << op << endl;
+		}
+		
+	}
+
+}
